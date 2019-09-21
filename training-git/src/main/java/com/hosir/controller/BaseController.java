ce4d3f50d46101f25fd9891fecb9aa1fd3d2fca4
Hosir <lxhthz@sina.com>
2019-09-21 20:11:43 +0800

    “[r1]反馈数据

--- training-git/src/main/java/com/hosir/controller/BaseController.java
+++ training-git/src/main/java/com/hosir/controller/BaseController.java
@@ -10,6 +10,11 @@ public class BaseController {
     Map<String,Object> result = new HashMap<>();
     result.put("status", true);
     result.put("state", false);
+	Map<String,Object> data = new HashMap<>();
+	data.put("01002001","1");
+	data.put("01002002","2");
+	data.put("01002003","3");
+	result.put("data", data);
     return "";
   }
 
